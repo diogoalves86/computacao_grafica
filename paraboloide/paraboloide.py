e627fae318047042e278e311aebfb2b675fde0de
Diogo Alves <diogoalvessilva86@gmail.com>
2021-09-07 18:49:44 -0300

    Adicionando prints e alguns arquivos que não estavam no repositório remoto

--- paraboloide/paraboloide.py
+++ paraboloide/paraboloide.py
@@ -1,6 +1,11 @@
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from OpenGL.GL import *
+import math
+
+n1 = 50
+n2 = 50
+r = 2
 
 def f(x,y):
     return x**2-y**2
@@ -11,23 +16,28 @@ xf, yf = 2, 2
 dx, dy = (xf - x0)/M, (yf - y0)/N
 a = 0
 
+def f1(i,j):
+    theta = (math.pi*i/(n1-1))-(math.pi/2)
+    phi = 2*math.pi*j/(n2-1)
+    x = r*math.cos(theta)*math.cos(phi)
+    y = r*math.sin(theta)
+    z = r*math.cos(theta)*math.sin(phi)
+    return x,y**2,z
 
 def mesh():
     glPushMatrix()
     glRotatef(a,1.0,0.0,0.0)
-    glBegin(GL_POINTS)
-
+    glBegin(GL_QUAD_STRIP)
     
-    for i in range(0, N):
-        y = y0 + i*dy
-        for j in range(0,M):
-            x = x0 + j*dx
-            z = f(x,y)
+    for i in range(0,n1//2): 
+        for j in range(0,n2): 
+            glColor3f(1.0-(i/n1), i/n1, 255)
+            x,y,z = f1(i,j)
             glVertex3f(x,y,z)
-    
+            x,y,z = f1(i+1,j)
+            glVertex3f(x,y,z)
     glEnd()
     glPopMatrix()
-
 
 def desenha():
     global a
@@ -35,7 +45,7 @@ def desenha():
     mesh()
     a+=1
     glutSwapBuffers()
-  
+
 def timer(i):
     glutPostRedisplay()
     glutTimerFunc(10,timer,1)
@@ -43,12 +53,12 @@ def timer(i):
 glutInit(sys.argv)
 glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_MULTISAMPLE)
 glutInitWindowSize(600,600)
-glutCreateWindow("Nuvem De Pontos")
+glutCreateWindow("Paraboloide")
 glutDisplayFunc(desenha)
 glEnable(GL_MULTISAMPLE)
 glEnable(GL_DEPTH_TEST)
-glClearColor(0,0,0,1)
-gluPerspective(40,600.0/600.0,0.1,200.0)
+glClearColor(0.2,0,0.2,0)
+gluPerspective(50,300.0/600.0,0.1,100.0)
 glTranslatef(0.0,0.0,-10)
 glutTimerFunc(10,timer,1)
 glutMainLoop()
